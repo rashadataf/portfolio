539c59c89aadfd83644b1cc3115464aaca89f513
Rashad Ataf <rashadataf@gmail.com>
2023-12-18 02:39:55 +0000

    feat: customize tailwind colors + implement ThemeProvider

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,11 +9,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      textColor: {
+        main: "var(--text-color)",
       },
+      backgroundColor: {
+        main: "var(--bg-color)",
+      },
+      colors: {
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        accent: "var(--accent-color)"
+      }
     },
   },
   plugins: [],
