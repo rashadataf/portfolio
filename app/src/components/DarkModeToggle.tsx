a0c3dd0a72f38b54492efb99a80e4f408ab4eaa7
Rashad Ataf <rashadataf@gmail.com>
2023-12-21 00:22:56 +0000

    feat: add dark and light mode animated icons

--- app/src/components/DarkModeToggle.tsx
+++ app/src/components/DarkModeToggle.tsx
@@ -1,15 +1,24 @@
 'use client';
 import { useThemeContext } from '@/context/theme.provider';
+import { THEME } from '@/types';
+import { MoonIcon, SunIcon } from './Icons';
+
+const renderIconConditionally: {
+    [key in THEME]: JSX.Element;
+} = {
+    "dark": <SunIcon />,
+    "light": <MoonIcon />,
+}
 
 export const ThemeToggler = () => {
-    const { toggleTheme } = useThemeContext();
+    const { toggleTheme, theme } = useThemeContext();
 
     return (
         <button
             onClick={toggleTheme}
             className="text-primary hover:text-main focus:outline-none"
         >
-            Dark/Light
+            {renderIconConditionally[theme]}
         </button>
     );
 };
