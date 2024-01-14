8fdc65047a3bb5043d48abc43dcd4be368a98df6
Rashad Ataf <rashadataf@gmail.com>
2024-01-14 16:45:23 +0000

    Improve entrypoint.sh to kkep nginx container running

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 envsubst '$$DOMAIN_NAME' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-nginx
+nginx &
 
-sleep 10
+sleep 15
 
 certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
 
 nginx -s reload
+
+# Tail Nginx logs or run a sleep loop
+tail -f /var/log/nginx/access.log
