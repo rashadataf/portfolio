0cb1e1d37f2de0cfeb267b836beaa19f7253e7ca
Rashad Ataf <rashadataf@gmail.com>
2024-08-02 09:31:21 +0100

    feat: add certbot as independent service

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -11,14 +11,14 @@ nginx &
 # Wait for Nginx to start
 sleep 15
 
-if [ "$NODE_ENV" = "production" ]; then
-  certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
-  nginx -s reload
+# if [ "$NODE_ENV" = "production" ]; then
+#   certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
+#   nginx -s reload
 
-  # Set up a cron job to renew certificates
-  echo "0 0 * * * /opt/certbot/bin/certbot renew --quiet --renew-hook 'nginx -s reload'" | crontab -
-  cron
-fi
+#   # Set up a cron job to renew certificates
+#   echo "0 0 * * * /opt/certbot/bin/certbot renew --quiet --renew-hook 'nginx -s reload'" | crontab -
+#   cron
+# fi
 
 # Tail Nginx logs or run a sleep loop
 tail -f /var/log/nginx/access.log
