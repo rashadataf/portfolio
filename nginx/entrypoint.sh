54b098ddc9bfe5d98008a493ed7455c3d172ae6d
Rashad Ataf <rashadataf@gmail.com>
2024-08-02 01:30:44 +0100

    fix: nginx config files

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
-envsubst '$$DOMAIN_NAME' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+if [ "$NODE_ENV" = "production" ]; then
+  envsubst '$$DOMAIN_NAME' < /etc/nginx/templates/production.conf.template > /etc/nginx/conf.d/default.conf
+else
+  envsubst '$$DOMAIN_NAME' < /etc/nginx/templates/dev.conf.template > /etc/nginx/conf.d/default.conf
+fi
 
 nginx &
 
+# Wait for Nginx to start
 sleep 15
 
-certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
+if [ "$NODE_ENV" = "production" ]; then
+  certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
+  nginx -s reload
 
-nginx -s reload
+  # Set up a cron job to renew certificates
+  echo "0 0 * * * /opt/certbot/bin/certbot renew --quiet --renew-hook 'nginx -s reload'" | crontab -
+  cron
+fi
 
 # Tail Nginx logs or run a sleep loop
 tail -f /var/log/nginx/access.log
