16d5cdb0f54c0a36cea3b17e21f66efca440de08
Rashad Ataf <rashadataf@gmail.com>
2024-08-31 19:44:23 +0100

    fix: nginx letsencrypt

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p ${CHALLENGE_DIR}
+
 if [ "$NODE_ENV" = "production" ]; then
   envsubst '$$DOMAIN_NAME$$PORT' < /etc/nginx/templates/production.conf.template > /etc/nginx/conf.d/default.conf
 else
@@ -11,5 +13,7 @@ nginx &
 sleep 15
 
 nginx -s reload
+
+sleep 15
 
 tail -f /var/log/nginx/access.log
