53017dd888562720f652278b27fc5412c481b7ca
Rashad Ataf <rashadataf@gmail.com>
2024-08-31 16:54:38 +0100

    improve dynamic docker implementation

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
 if [ "$NODE_ENV" = "production" ]; then
-  envsubst '$$DOMAIN_NAME' < /etc/nginx/templates/production.conf.template > /etc/nginx/conf.d/default.conf
+  envsubst '$$DOMAIN_NAME$$PORT' < /etc/nginx/templates/production.conf.template > /etc/nginx/conf.d/default.conf
 else
-  envsubst '$$DOMAIN_NAME' < /etc/nginx/templates/dev.conf.template > /etc/nginx/conf.d/default.conf
+  envsubst '$$DOMAIN_NAME$$PORT' < /etc/nginx/templates/dev.conf.template > /etc/nginx/conf.d/default.conf
 fi
 
 nginx &
 
-# Wait for Nginx to start
 sleep 15
 
-# if [ "$NODE_ENV" = "production" ]; then
-#   certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
-#   nginx -s reload
+nginx -s reload
 
-#   # Set up a cron job to renew certificates
-#   echo "0 0 * * * /opt/certbot/bin/certbot renew --quiet --renew-hook 'nginx -s reload'" | crontab -
-#   cron
-# fi
-
-# Tail Nginx logs or run a sleep loop
 tail -f /var/log/nginx/access.log
