a8cf4c488f5bba830df119c65f6a5b5d2ec64507
Rashad Ataf <rashadataf@gmail.com>
2024-10-27 12:01:36 +0000

    Improve code (#4)
    * feat: git rid of all values
    
    * upgrade packages + node version
    
    * fix: nginx config files
    
    * feat: add certbot as an independent service
    
    * improve dynamic docker implementation
    
    * feat: add configs for the well-known
    
    * fix: mkdir only for nginx
    
    * fix: nginx letsencrypt
    
    * feat: add scripts to automate deployment
    
    * feat: separate nginx
    
    * fix: nginx old service
    
    * fix: docker production
    
    * fix: docker start and restart scripts
    
    * docs: update README with project overview and scripts documentation

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
-envsubst '$$DOMAIN_NAME' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+mkdir -p ${CHALLENGE_DIR}
+
+if [ "$NODE_ENV" = "production" ] && [ "$WITH_CERTBOT" = "true" ]; then
+  envsubst '$$DOMAIN_NAME$$PORT' < /etc/nginx/templates/production.conf.template > /etc/nginx/conf.d/default.conf
+  nginx &
+  sleep 15
+  certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
+  nginx -s reload
+elif [ "$RESET_CONFIG" = "true" ]; then
+  if [ "$NODE_ENV" = "production" ]; then
+    envsubst '$$DOMAIN_NAME$$PORT' < /etc/nginx/templates/production.conf.template > /etc/nginx/conf.d/default.conf
+  else
+    envsubst '$$DOMAIN_NAME$$PORT' < /etc/nginx/templates/dev.conf.template > /etc/nginx/conf.d/default.conf
+  fi
+  echo "Nginx configuration reloaded with new template settings"
+fi
+
+if [ ! -f /etc/nginx/conf.d/default.conf ]; then
+  echo "Error: Nginx configuration file /etc/nginx/conf.d/default.conf is missing."
+  exit 1
+fi
 
 nginx &
 
 sleep 15
-
-certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --agree-tos --non-interactive --email $EMAIL_ADDRESS
-
-nginx -s reload
-
-# Tail Nginx logs or run a sleep loop
 tail -f /var/log/nginx/access.log
---
+++ scripts.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+function dev() {
+  RESET_CONFIG=true docker compose --profile dev up
+}
+
+function dev_reset() {
+  RESET_CONFIG=false docker compose --profile dev up
+}
+
+function restart_production() {
+  WITH_CERTBOT=false RESET_CONFIG=false docker compose --profile production up --build --detach
+}
+
+function production_ssl() {
+  WITH_CERTBOT=true RESET_CONFIG=true docker compose --profile production up --build --detach
+}
+
+function production_no_ssl() {
+  WITH_CERTBOT=false RESET_CONFIG=true docker compose --profile production up
+}
+
+function nginx_reload() {
+  docker compose exec nginx nginx -s reload
+}
+
+function clear_docker() {
+  echo "Stopping all containers..."
+  docker compose down
+
+  echo "Prune docker builder..."
+  docker builder prune -f -a
+  
+  echo "Prune docker system..."
+  docker system prune -f -a
+
+  echo "Removing all networks not used by at least one container..."
+  docker network prune -f
+  
+  echo "Removing all unused volumes..."
+  docker volume rm $(docker volume ls -q)
+  
+  echo "Docker environment is clean."
+}
+
+function clear_all() {
+  clear_docker
+
+  echo "Removing node_modules folder ..."
+  rm -rf node_modules
+
+  echo "Removing certs folder ..."
+  rm -rf certs
+
+  echo "Removing certs data folder ..."
+  rm -rf data
+
+  echo "Removing logs folder ..."
+  rm -rf logs
+
+  echo "Removing nginx config folder ..."
+  rm -rf nginx/conf
+}
+
+"$@"
