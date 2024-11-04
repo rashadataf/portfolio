93466d112c010ceb7c1e4b8587328cbd8ec4b0f3
Rashad Ataf <rashadataf@gmail.com>
2024-11-04 00:27:52 +0000

    fix: Hydration failed error

--- app/src/components/DarkModeToggle.tsx
+++ app/src/components/DarkModeToggle.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useState } from 'react';
 import { useThemeContext } from '@/context/theme.provider';
 import { THEME } from '@/types';
 import { MoonIcon, SunIcon } from '@/components/Icons';
@@ -16,6 +17,11 @@ type Props = {
 
 export const ThemeToggler = ({ className }: Props) => {
     const { toggleTheme, theme } = useThemeContext();
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
     return (
         <button
@@ -23,7 +29,7 @@ export const ThemeToggler = ({ className }: Props) => {
             className={`text-secondary hover:text-accent focus:outline-none focus:border-primary ${className}`}
             aria-label={`The ${theme} Mode Is Active, Press to switch mode`}
         >
-            {renderIconConditionally[theme]}
+            {isMounted ? renderIconConditionally[theme] : null}
         </button>
     );
 };
