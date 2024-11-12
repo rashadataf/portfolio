ad66e7204dd5e21adb58caba76aca639627a6fff
Rashad Ataf <rashadataf@gmail.com>
2024-11-12 23:53:38 +0000

    fix: params type for articles route

--- app/src/app/api/articles/[id]/route.ts
+++ app/src/app/api/articles/[id]/route.ts
@@ -1,13 +1,14 @@
-import { NextRequest } from 'next/server';
 import { ArticleController } from '@/modules/article/article.controller';
 
 const articleController = new ArticleController();
 
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    return articleController.getArticleById(params.id);
+export async function GET(_: unknown, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
+    return articleController.getArticleById(id);
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-    return articleController.deleteArticle(params.id);
+export async function DELETE(_: unknown, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
+    return articleController.deleteArticle(id);
 }
