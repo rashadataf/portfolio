f3479574b04120fc1c297ac5a29ac22896141f39
Rashad Ataf <rashadataf@gmail.com>
2025-01-22 01:31:44 +0000

    test: build with profile

--- scripts.sh
+++ scripts.sh
@@ -9,7 +9,7 @@ function restart_dev() {
 }
 
 function restart_production() {
-  WITH_CERTBOT=false RESET_CONFIG=false docker compose build --build-arg CACHE_BUST=$(date +%s) --no-cache
+  WITH_CERTBOT=false RESET_CONFIG=false docker compose --profile production build --no-cache --build-arg CACHE_BUST=$(date +%s)
   WITH_CERTBOT=false RESET_CONFIG=false docker compose --profile production up --build --detach
 }
 
