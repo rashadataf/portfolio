f153e302286c7b5c137fde0971dfbc1afc776443
Rashad Ataf <rashadataf@gmail.com>
2025-01-28 22:21:17 +0000

    test

--- app/src/app/(admin)/admin/articles/[id]/page.tsx
+++ app/src/app/(admin)/admin/articles/[id]/page.tsx
@@ -1,17 +1,16 @@
-import dynamic from 'next/dynamic';
-import { Loader } from '@/components/Loader';
+import { ArticlePage } from '@/components/ArticlePage';
 
-const ArticlePageComponent = dynamic(() =>
-    import('@/components/ArticlePage').then((mod) => mod.ArticlePage),
-    {
-        loading: () => <Loader />,
-    }
-)
-export default async function ArticlePage({
+// const ArticlePageComponent = dynamic(() =>
+//     import('@/components/ArticlePage').then((mod) => mod.ArticlePage),
+//     {
+//         loading: () => <Loader />,
+//     }
+// )
+export default async function AdminArticlePage({
     params,
 }: {
     params: Promise<{ id: string }>
 }) {
     const id = (await params).id;
-    return <ArticlePageComponent editable={true} articleId={id} />
+    return <ArticlePage editable={true} articleId={id} />
 }
