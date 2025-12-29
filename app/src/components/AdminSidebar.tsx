b7d7eff10c62322381c90fdba5e6e66908ee2a76
Rashad Ataf <rashadataf@gmail.com>
2025-12-29 18:26:51 +0000

    feat: add file manager (#29)

--- app/src/components/AdminSidebar.tsx
+++ app/src/components/AdminSidebar.tsx
@@ -95,6 +95,15 @@ export const AdminSidebar = ({
                     </li>
                     <li>
                         <Link
+                            href="/admin/files"
+                            className={`block p-2 hover:bg-gray-700 ${isActive('/admin/files') ? 'bg-gray-700' : ''
+                                }`}
+                        >
+                            Files
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
                             href="/admin/articles/drafts"
                             className={`block p-2 hover:bg-gray-700 ${isActive('/admin/articles/drafts') ? 'bg-gray-700' : ''
                                 }`}
