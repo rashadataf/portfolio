2c396750f4f3a3d6e4c4b8161b9602c0a6543cb3
Rashad Ataf <rashadattaf@gmail.com>
2025-12-29 20:35:22 +0000

    fix: allow https domain name as image url source

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rashadataf.com',
+        pathname: '/api/files/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.rashadataf.com',
+        pathname: '/api/files/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
